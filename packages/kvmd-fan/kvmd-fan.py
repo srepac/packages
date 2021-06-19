5f907892bee134f15106d2018b72bfc6f6395307
srepac <68381903+srepac@users.noreply.github.com>
2021-06-19 09:31:24 -0500

    Update kvmd-fan.py

--- packages/kvmd-fan/kvmd-fan.py
+++ packages/kvmd-fan/kvmd-fan.py
@@ -9,7 +9,7 @@
 # You need to modify the service to run this script instead which varies PWM
 # ... fan level depending on temperature:
 #
-# ExecStart=/usr/bin/python3 /usr/bin/kvmd-fan.py &
+# ExecStart=/usr/bin/python3 /usr/bin/kvmd-fan.py
 #
 # The final kvmd-fan.service file should look like this when you are done:
 """ 
@@ -19,7 +19,7 @@ After=systemd-modules-load.service
  
 [Service]
 Type=simple
-ExecStart=/usr/bin/python3 /usr/bin/kvmd-fan.py &
+ExecStart=/usr/bin/python3 /usr/bin/kvmd-fan.py
  
 [Install]
 WantedBy=multi-user.target
@@ -42,36 +42,30 @@ while True:
         if temp <= 37:
             level_temp = 37
             PWM = 75
-            os.popen(" /bin/bash -c 'gpio -g mode 12 pwm && gpio -g pwm 12 75' ")
         elif temp <= 41:
             level_temp = 41
             PWM = 100
-            os.popen(" /bin/bash -c 'gpio -g mode 12 pwm && gpio -g pwm 12 100' ")
         elif temp <= 45:
             level_temp = 45
             PWM = 125
-            os.popen(" /bin/bash -c 'gpio -g mode 12 pwm && gpio -g pwm 12 125' ")
         elif temp <= 47:
             level_temp = 47
             PWM = 150
-            os.popen(" /bin/bash -c 'gpio -g mode 12 pwm && gpio -g pwm 12 150' ")
         elif temp <= 49:
             level_temp = 49
             PWM = 175
-            os.popen(" /bin/bash -c 'gpio -g mode 12 pwm && gpio -g pwm 12 175' ")
         elif temp <= 51:
             level_temp = 51
             PWM = 200
-            os.popen(" /bin/bash -c 'gpio -g mode 12 pwm && gpio -g pwm 12 200' ")
         elif temp <= 53:
             level_temp = 53
             PWM = 225
-            os.popen(" /bin/bash -c 'gpio -g mode 12 pwm && gpio -g pwm 12 225' ")
         else:
             level_temp = 55
             PWM = 250
-            os.popen(" /bin/bash -c 'gpio -g mode 12 pwm && gpio -g pwm 12 250' ")
 
-    print(DATE, CPU_TEMP, PWM)
+        os.popen(' /bin/bash -c "gpio -g mode 12 pwm && gpio -g pwm 12 {0}" '.format(PWM))
+        print(DATE, CPU_TEMP, PWM)
+
     time.sleep(1)
 
